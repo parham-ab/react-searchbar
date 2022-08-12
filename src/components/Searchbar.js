cc15ebcaabb7dbf349db629bdecd757bdb827cf2
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-12 12:06:33 +0430

    store filtered data on state

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const Searchbar = ({data}) => {
-    console.log(data);
-    return (
-        <div>
-            
-        </div>
-    );
+const Searchbar = ({ data }) => {
+  const [inputVal, setInputVal] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+
+  // filterHandle
+  const filterHandle = (e) => {
+    setInputVal(e.target.value);
+    const filteredItems = data.filter((item) => {
+      return item.title.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    console.log(filteredItems);
+
+    inputVal === "" ? setFilteredData([]) : setFilteredData(filteredItems);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={inputVal}
+        onChange={filterHandle}
+      />
+    </div>
+  );
 };
 
 export default Searchbar;
